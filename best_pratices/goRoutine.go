e1e9d6fbccdae5272bc2ff47217fa5e44d5b9b44
Diogo Amaral <diogocoelhoamaral@gmail.com>
2025-02-08 23:48:12 +0000

    start project

---
+++ best_pratices/goRoutine.go
@@ -0,0 +1,22 @@
+package best_pratices
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Goroutines are lightweight, but excessive spawning can lead to high memory usage.
+// Use sync.WaitGroup to manage goroutines efficiently.
+// Key concept: Avoid common pitfalls like capturing loop variables incorrectly in goroutines.
+func GoRoutine() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fmt.Println("Goroutine", i)
+		}(i)
+	}
+	wg.Wait()
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "GoBestPratices/best_pratices"
+
+func main() {
+	best_pratices.GoRoutine()
+}
