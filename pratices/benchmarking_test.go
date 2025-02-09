1128555bc7191d33a932c89291bef887c26b52ea
Diogo Amaral <diogocoelhoamaral@gmail.com>
2025-02-09 00:35:08 +0000

    test

--- pratices/benchmarking_test.go
+++ pratices/benchmarking_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 //Use table-driven tests for readability.
 
-func BenchmarkAdd(t *testing.T) {
+// Test function (must start with "Test")
+func TestAdd(t *testing.T) {
 	tests := []struct {
 		a, b, expected int
 	}{
@@ -14,13 +15,14 @@ func BenchmarkAdd(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		result := add(tt.a, tt.b)
+		result := Add(tt.a, tt.b)
 		if result != tt.expected {
 			t.Errorf("Add(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
 		}
 	}
 }
 
-func add(a, b int) int {
+// Function to be tested
+func Add(a, b int) int {
 	return a + b
 }
